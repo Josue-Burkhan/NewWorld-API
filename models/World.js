6cd8c47625a3470ccae7753dcb8ff980ebf63ea6
joshua.burkhan <bai21030@byui.edu>
2025-05-27 21:51:07 -0500

    add a req.body in all put, and post routes V4.1 (change world model)

--- models/World.js
+++ models/World.js
@@ -9,15 +9,22 @@ const worldSchema = new mongoose.Schema({
   createdAt: { type: Date, default: Date.now },
 
   modules: {
+    characters: { type: Boolean, default: false },
+    locations: { type: Boolean, default: false },
+    factions: { type: Boolean, default: false },
+    items: { type: Boolean, default: false },
+    events: { type: Boolean, default: false },
+    languages: { type: Boolean, default: false },
     abilities: { type: Boolean, default: false },
     technology: { type: Boolean, default: false },
     powerSystem: { type: Boolean, default: false },
     creatures: { type: Boolean, default: false },
     religion: { type: Boolean, default: false },
-    ancientHistory: { type: Boolean, default: false },
+    story: { type: Boolean, default: false },
     races: { type: Boolean, default: false },
     economy: { type: Boolean, default: false }
   }
+
 });
 
 module.exports = mongoose.model('World', worldSchema);
